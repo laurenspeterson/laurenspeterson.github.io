f5d69ee9a3d17db62a048f72406860f490c741ea
laurenspeterson <bea17036@byui.edu>
2020-02-14 22:08:37 -0700

    done

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -1,9 +1,9 @@
-let temp = parseFloat(document.getElementById('temp').innerHTML);
+ let temp = parseFloat(document.getElementById('temp').innerHTML);
 let wind = parseFloat(document.getElementById('wind').innerHTML);
 if (temp > 50 || wind < 3.0) {
    document.getElementById('windchill').innerHTML = "N/A";
 } else {
-   let chill = 35.74 + (0.6215 * temp) - (35.75 * wind**0.16) + 
-   (0.4275 * (temp * wind**0.16));
-   document.getElementById('windchill').innerHTML = chill.toFixed(0);
+   let chill = 35.74 + (0.6215 * temp) - (35.75 * (wind**0.16)) + 
+   (0.4275 * (temp * (wind**0.16)));
+   document.getElementById('windchill').innerHTML = chill.toFixed(0); 
 }

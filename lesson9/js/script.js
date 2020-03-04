60c02cf86c33e78383082d19d788dd144d7aac50
laurenspeterson <bea17036@byui.edu>
2020-03-04 15:20:31 -0700

    new

--- lesson9/js/script.js
+++ lesson9/js/script.js
@@ -1,6 +1,3 @@
-var lastModif = new Date(document.lastModified);
-    document.getElementById("lastModified").textContent=lastModif;
-
 function onClickMenu(){
 	document.getElementById("menu").classList.toggle("change");
 	document.getElementById("nav").classList.toggle("change");

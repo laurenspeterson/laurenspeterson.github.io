5ccc25a09d5039226cbf1914cb32f38f57a55869
laurenspeterson <bea17036@byui.edu>
2020-03-19 19:12:48 -0600

    new

--- lesson11/js/preston/events.js
+++ lesson11/js/preston/events.js
@@ -1,21 +1,22 @@
 const requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+
 fetch(requestURL)
   .then(function (response) {
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject); 
+    console.table(jsonObject);
     const towns = jsonObject['towns']; 
 
-    for (let i = 0; i < towns.length; i++) {
-      if (towns[i].name == "Preston")
-      
-        let list = document.createElement('ul');
+    for (let i = 0; i < towns.length; i++ ) {
+      if (towns[i].name == "Preston") {
+        
+        let list = document.createElement('ul'); 
 
-    for (let y = 0; y < towns[i].events.length; y++) {
-        let item = document.createElement('li');
-        item.textContent = "Preston Events: " +towns[i].events[y];
-        list.appendChild(item);
-       
-       document.querySelector('div.list').appendChild(list);
-   } } } );
+        for (let y = 0; y < towns[i].events.length; y++) {
+          let item = document.createElement('li');
+          item.textContent = towns[i].events[y];
+          list.appendChild(item);
+        } 
+document.querySelector('div.list').appendChild(list); 
+      } } });
